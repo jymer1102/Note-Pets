ff6543844c811b2ca9dc55dfa07a54fc974abae2
Jackson Weimer <jacksonaweimer@icloud.com>
2026-08-15 08:41:35 -0500

    Update Supabase URL to production endpoint

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_URL = "https://ckjwbcwwrbtyqqliesnk.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrandiY3d3cmJ0eXFxbGllc25rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3Nzk4ODAsImV4cCI6MjEwMjM1NTg4MH0.XD932PhYXoJ5psXWKDttTJuTHdYjun6hMT3MlgsQs7E";
 
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
